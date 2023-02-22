bfcb9e52187730023fea493a14b4f35f99bddae2
Philip Zalewski <jochotyler1@gmail.com>
2023-02-22 13:01:21 +0100

    hoover effect

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,7 +4,11 @@ import '../css/ProductCard.css'
 export default function ProductCard(props){
     return (
         <div className="product-card">
-            <img className="image" src={props.item.imageURL} alt="image"/>
+            <div className="hover-rotate">
+                <figure>
+                    <img src={props.item.imageURL} alt="image"/>
+                </figure>
+            </div>
             <div className="description">
                 <h2 className="title">{props.item.title}</h2>
                 <h3 className="gender">{props.item.gender}</h3>
